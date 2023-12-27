2297286a0c10e33f1d7a98f1b92843f8794ddbb7
Vijayakumar G <vijayakumar.gunasekaran@procaresoftware.com>
2023-12-27 12:28:57 +0530

    Initil commit for demo project

---
+++ csv_poc/lib/database_helper.dart
@@ -0,0 +1,31 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+const String TABLE_NAME_PATIENT = "patient";
+const String PATIENT_TABLE_COLUMNS =
+    "(patient_track_id TEXT,gender TEXT,age TEXT,weight TEXT,created_at TEXT,avg_diastolic TEXT,next_medical_review_date TEXT,next_bp_assesment_date TEXT,bmi TEXT,last_assessment_date TEXT,avg_systolic TEXT,enrollment_at TEXT,enrollment_at_red TEXT,screening_referral TEXT,is_confirm_diagnosis TEXT,is_red_risk_patient TEXT,is_pregnant TEXT,cvd_risk_level TEXT,glucose_type TEXT,screening_id TEXT,is_screening TEXT,patient_status TEXT,patient_id TEXT,tenant_id TEXT,updated_at TEXT,program_id TEXT,glucose_value TEXT,sc_name TEXT,country_name TEXT,county_name TEXT,sub_county_name TEXT,is_deleted_s TEXT,city TEXT,is_active TEXT,name TEXT,confirm_diagnosis TEXT,comorbidity_name TEXT,comorbidity TEXT,other_comorbidity TEXT,diabetes_year_of_diagnosis TEXT,diabetes_patient_type TEXT,htn_patient_type TEXT,diabetes_diagnosis TEXT,htn_year_of_diagnosis TEXT,is_diabetes_diagnosis TEXT,is_htn_diagnosis TEXT,diabetes_diag_controlled_type TEXT,created_at_s TEXT,category_s TEXT,glucose_value_s TEXT,glucose_type_s TEXT,avg_diastolic_long_s TEXT,avg_systolic_long_s TEXT,is_before_htn_diagnosis_s TEXT,is_before_diabetes_diagnosis_s TEXT,referredreason TEXT,diabetes_type_s TEXT,diagnosis TEXT,enrolledcondition TEXT,p_name TEXT,provisional_diagnosis TEXT,insurance_type TEXT,insurance_status TEXT,insurance_type_case TEXT,htn_new_vs_existing_diagnoses TEXT,dbm_new_vs_existing_diagnoses TEXT,pregnant_status TEXT)";
+const String PATIENT_TABLE_INSERT_VALUE =
+    "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+class DataBaseHelper {
+  DataBaseHelper._();
+
+  static const String dbName = "bi_db";
+  static final DataBaseHelper instance = DataBaseHelper._();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await initDataBase();
+    return _database!;
+  }
+
+  Future<Database> initDataBase() async {
+    String path = join(await getDatabasesPath(), dbName);
+    return openDatabase(path, version: 1, onCreate: (db, version) async {
+      //create tables
+      db.execute("CREATE TABLE $TABLE_NAME_PATIENT $PATIENT_TABLE_COLUMNS");
+    });
+  }
+}
